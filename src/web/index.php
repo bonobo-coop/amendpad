a20c38f5965c7846543849a5960a6ad350ce29e2
Pepe <pepe.84@gmail.com>
2014-02-27 14:09:49 +0100

    Bootstrap theme + Home page

--- src/web/index.php
+++ src/web/index.php
@@ -10,13 +10,15 @@ $app['debug'] = true;
 
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__ . '/../data/main.log',
+))->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/views',
 ));
 
 /**
  * Router + Controller
  */
 $app->get('/', function () use ($app) {
-    return "Hello World";
+    return $app['twig']->render('home.twig');
 });
 
 $app->run();
